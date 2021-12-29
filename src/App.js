3b70ac5699d1af389a0f1b780e73bb31de593861
nicopeia <n.peianovich@gmail.com>
2021-12-28 22:40:34 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+<p>
+         TIENDAWEB NICOPEIA
+          
+        </p>
+     
+    </div>
+  );
+}
+
+export default App;
